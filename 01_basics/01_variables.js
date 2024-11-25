fd5766d72fd30013990e71daf14de988a929e047
SushreeDebashree <150846603+SushreeDebashree@users.noreply.github.com>
2024-11-25 20:08:28 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId=123456
+let accountEmail="contact2sushree@gmail.com"
+var accountPassword="12345"
+accountCity="Bangalore"
+let accountState;
+
+// accountId = 2//not allowed
+accountEmail="mail2sdebashree@gmail.com"
+accountPassword="121212"
+accountCity="Bhubaneswar"
+console.log(accountId);
+/*
+Prefer not to use var because of issue in block scope and functional scope
+
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict";//treat all JS code as newer version
+
+//alert(3+3)// we are using node.js, not browser
+
+console.log(3+3)
+console.log("Sushree")
+
+let name="Sushree"
+let age=28
+let isLoggedIn=false
+let state;
+
+// number=> 2 to power 53
+// bigInt
+// string=> ""
+//boolean=> true/false
+//null==>standalone value
+//undefined=>
+//symbol=> unique
+
+
+//object
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
+
