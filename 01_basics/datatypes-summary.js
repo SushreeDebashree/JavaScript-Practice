926b4f77b6e3182d865710093ba1ed2ba467a860
SushreeDebashree <150846603+SushreeDebashree@users.noreply.github.com>
2024-11-26 18:59:18 +0000

    datatypes-summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id===anotherId);
+//console.log(id===anotherId);
 
 const bifNumber = 3455796089673957594n
 
@@ -30,12 +30,38 @@ const myFunction = function(){
     console.log(("Hello World"));
     
 }
-console.log(typeof bigNumber);
-console.log(typeof myFunction);
-console.log(typeof anotherId);
+//console.log(typeof bigNumber);
+//console.log(typeof myFunction);
+//console.log(typeof anotherId);
 
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack (Primitive), Heap(Non-Primitive)
+
+let myYoutubename = "aglimpseofsushree"
+let anothername = myYoutubename
+anothername = "Sushree"
+
+//console.log(myYoutubename);
+//console.log(anothername); 
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "sushree@google.com"
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
+
 
 
 
